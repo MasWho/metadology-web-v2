25e78f1f06fb90c2f4a3ad69e91fa396c88c4521
Mason Hu <xm.hu.wk@gmail.com>
2023-11-19 10:58:45 +0200

    carousel done with responsive design

--- components/carousel/large-image-carousel/large-image-carousel.tsx
+++ components/carousel/large-image-carousel/large-image-carousel.tsx
@@ -1,49 +1,174 @@
 import Image from 'next/image';
-import React, { DOMElement, Dispatch, MutableRefObject, PropsWithChildren, SetStateAction, createContext, useCallback, useContext, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, {
+  DOMElement,
+  Dispatch,
+  MutableRefObject,
+  PropsWithChildren,
+  SetStateAction,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import { motion, useInView, useMotionValueEvent, useScroll, useTransform } from 'framer-motion';
 import useWindowDimensions from '@/hooks/use-window-dimensions';
 import { DESKTOP_SCREEN_SIZE, TABLET_SCREEN_SIZE } from '../constants';
+import ChevronArrow from '@/components/arrows/chevron-arrow';
 
 const pictureSize = (width: number) => {
-  if(width < DESKTOP_SCREEN_SIZE) {
+  if (width < DESKTOP_SCREEN_SIZE) {
     return 400;
   }
 
-  if(width < TABLET_SCREEN_SIZE) {
+  if (width < TABLET_SCREEN_SIZE) {
     return 250;
   }
 
   return 600;
-}
+};
 
-const CarouselItemText = (props: { text: string }) => {};
+const CarouselItemText = (props: { items: { text: string }[] }) => {
+  const { items } = props;
+  const { currentFocusedItemIndex } = useCarouselContext();
+  const itemTexts = items.map((item, idx) => {
+    const itemOpacity = idx === currentFocusedItemIndex ? 1 : 0;
+    return (
+      <motion.p
+        key={`carousel-item-text-${idx}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: itemOpacity }}
+        transition={{ duration: 0.5 }}
+        className="absolute text-[14px] tablet:text-[16px]"
+      >
+        {item.text}
+      </motion.p>
+    );
+  });
+  return (
+    <article className="relative text-c-section-heading mb-[10px] w-[90vw] mx-auto lg-phone:mx-0 lg-phone:w-[300px] lg-phone:left-[calc(50vw-125px)] h-[100px] tablet:mb-[20px] tablet:w-[300px] tablet:h-[100px] tablet:left-[calc(50vw-200px)] desktop:left-[calc(50vw-305px)]">
+      {itemTexts}
+    </article>
+  );
+};
 
-const CarouselItemCaption = (props: { text: string }) => {};
+const CarouselItemCaption = (props: { items: { caption: string }[] }) => {
+  const { items } = props;
+  const { currentFocusedItemIndex } = useCarouselContext();
+  const itemCaptions = items.map((item, idx) => {
+    const itemOpacity = idx === currentFocusedItemIndex ? 1 : 0;
+    return (
+      <motion.p
+        key={`carousel-item-caption-${idx}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: itemOpacity }}
+        transition={{ duration: 0.5 }}
+        className="absolute text-[16px]"
+      >
+        {item.caption}
+      </motion.p>
+    );
+  });
+  return (
+    <div className='absolute w-[100vw] mt-[10px]'>
+      <article className="relative text-c-section-heading h-[24px] mx-auto flex justify-center">
+        {itemCaptions}
+      </article>
+    </div>
+  );
+};
 
-const CarouselNav = () => {};
+const CarouselNav = (props: { items: { caption: string }[] }) => {
+  const { items } = props;
+  const { currentFocusedItemIndex, setCurrentFocusedItemIndex } = useCarouselContext();
+  const isFirstImage = currentFocusedItemIndex === 0;
+  const isLastImage = currentFocusedItemIndex === items.length - 1;
 
-const CarouselItem = (props: { item: { url: string; text: string }; itemIndex: number; getCarouselRef?: () => MutableRefObject<any> }) => {
+  const changeImageHandler = (type: 'inc' | 'dec') => {
+    setCurrentFocusedItemIndex((prev: number) => {
+      let nextIndex = prev;
+      if (type === 'inc') {
+        nextIndex++;
+        return Math.min(nextIndex, items.length - 1);
+      } else {
+        nextIndex--;
+        return Math.max(0, nextIndex);
+      }
+    });
+  };
+
+  return (
+    <div className="relative w-[100vw] mt-[45px] desktop:left-[72vw] tablet:left-[75vw] tablet:mt-[5px] flex justify-center gap-10 tablet:gap-3 tablet:w-[10%]">
+      <ChevronArrow
+        direction="left"
+        size="30px"
+        inactive={isFirstImage}
+        onClick={changeImageHandler.bind(null, 'dec')}
+      />
+      <ChevronArrow
+        direction="right"
+        size="30px"
+        inactive={isLastImage}
+        onClick={changeImageHandler.bind(null, 'inc')}
+      />
+    </div>
+  );
+};
+
+const CarouselItem = (props: {
+  item: { url: string; text: string };
+  itemIndex: number;
+  getCarouselRef?: () => MutableRefObject<any>;
+}) => {
   const { item, itemIndex } = props;
+  const { setCurrentFocusedItemIndex, currentFocusedItemIndex } = useCarouselContext();
+  const prevIndexRef = useRef(currentFocusedItemIndex);
   const itemRef = useRef<any>(null);
-  const {width} = useWindowDimensions();
-  const offset = width! * 0.5 + pictureSize(width!) / 2 // Halft of image width
+  const { width } = useWindowDimensions();
+  const offset = width! * 0.5 + pictureSize(width!) / 2; // Halft of image width
   const negOffset = width! * 0.5 - pictureSize(width!) / 2;
-  const {carouselRef} = useCarouselContext();
-  const {scrollXProgress} = useScroll({
+  const { carouselRef } = useCarouselContext();
+  const { scrollXProgress } = useScroll({
     container: carouselRef,
     target: itemRef,
     axis: 'x',
     offset: [`start ${offset}px`, `end ${offset}px`, `start ${negOffset}px`, `end ${negOffset}px`],
-    layoutEffect: false
+    layoutEffect: false,
   });
   const opacity = useTransform(scrollXProgress, [0, 0.35, 0.75, 1], [0.3, 1, 1, 0.3]);
   const pictureWidth = pictureSize(width!);
   const pictureHeight = pictureWidth / 1.3;
-  const height = useTransform(scrollXProgress, [0, 0.35, 0.75, 1], [0.8 * pictureHeight, pictureHeight, pictureHeight, pictureHeight]);
+  const height = useTransform(
+    scrollXProgress,
+    [0, 0.35, 0.75, 1],
+    [0.8 * pictureHeight, pictureHeight, pictureHeight, pictureHeight]
+  );
+
+  useEffect(() => {
+    if (currentFocusedItemIndex !== prevIndexRef.current && currentFocusedItemIndex === itemIndex) {
+      itemRef.current.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+    }
+    prevIndexRef.current = currentFocusedItemIndex;
+  }, [currentFocusedItemIndex]);
+
+  useMotionValueEvent(opacity, 'change', (latestOpacity) => {
+    if (latestOpacity === 1) {
+      setCurrentFocusedItemIndex(itemIndex);
+      prevIndexRef.current = itemIndex;
+    }
+  });
 
   return (
-    <li className="flex items-center snap-center w-[250px] h-[300px] tablet:w-[400px] tablet:h-[308px] desktop:w-[600px] desktop:h-[460px]" ref={itemRef} >
-      <motion.div style={{opacity, height: width! >= TABLET_SCREEN_SIZE ? height : undefined }} className="relative origin-left w-[100%] h-[100%]">
+    <li
+      className="flex items-center snap-center w-[250px] h-[300px] tablet:w-[400px] tablet:h-[308px] desktop:w-[600px] desktop:h-[460px]"
+      ref={itemRef}
+    >
+      <motion.div
+        style={{ opacity, height: width! >= TABLET_SCREEN_SIZE ? height : undefined }}
+        className="relative origin-left w-[100%] h-[100%]"
+      >
         <Image
           src={item.url}
           alt={`carousel image with caption: ${item.text}`}
@@ -63,21 +188,19 @@ type CarouselItemsProps = {
 };
 const CarouselItems = (props: CarouselItemsProps) => {
   const { images } = props;
-  const {carouselRef} = useCarouselContext();
+  const { carouselRef } = useCarouselContext();
 
   const itemList = images.map((item, idx) => {
-    return (
-      <CarouselItem
-        key={`explore-four-carousel-item-${idx}`}
-        item={item}
-        itemIndex={idx}
-      />
-    );
+    return <CarouselItem key={`explore-four-carousel-item-${idx}`} item={item} itemIndex={idx} />;
   });
 
   return (
     // Scroll container
-    <div id="large-image-carousel" className="overflow-scroll no-scrollbar snap-mandatory snap-x" ref={carouselRef}>
+    <div
+      id="large-image-carousel"
+      className="overflow-scroll no-scrollbar snap-mandatory snap-x"
+      ref={carouselRef}
+    >
       <ul className="relative flex w-fit gap-[10px] pl-[calc(50%-125px)] pr-[calc(50%-125px)] tablet:gap-[20px] tablet:pl-[calc(50%-200px)] tablet:pr-[calc(50%-200px)] desktop:pl-[calc(50%-300px)] desktop:pr-[calc(50%-300px)]">
         {itemList}
       </ul>
@@ -103,11 +226,11 @@ const LargeImageCarousel = (props: LargeImageCarouselProps) => {
   const ctxValue = {
     currentFocusedItemIndex,
     setCurrentFocusedItemIndex,
-    carouselRef
+    carouselRef,
   };
   return (
     <carouselContext.Provider value={ctxValue}>
-      <section>{children}</section>
+      <section className='pb-14'>{children}</section>
     </carouselContext.Provider>
   );
 };
--- components/sections/explore/sub-section-four.tsx
+++ components/sections/explore/sub-section-four.tsx
@@ -6,31 +6,38 @@ import { useEffect } from 'react';
 const images = [
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img1',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc1 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 1'
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img2',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc2 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 2'
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img3',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc3 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 3'
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img4',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc4 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 4'
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img5',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc5 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 5'
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img6',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc6 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 6'
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/explore-sub-section-four-img7',
-    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.' 
+    text: 'Lorem ipsum, dolor sit amet consectetur desc7 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
+    caption: 'Caption 7'
   },
 ];
 
@@ -61,7 +68,7 @@ const ExploreSubsectionFour = () => {
 
   return (
     <SectionLayout sectionName={'explore'} bgColor="bg-c-secondary" noPadding>
-      <div ref={scope} className="relative py-14 flex flex-col left-[2.5vw] mx-[2.5vw] tablet:left-[20vw] tablet:mx-[5vw]">
+      <div ref={scope} className="relative pt-14 pb-10 tablet:pb-14 flex flex-col left-[2.5vw] mx-[2.5vw] tablet:left-[20vw] tablet:mx-[5vw]">
         {/* Very custom Heading */}
         <h1 className="text-[20px] tablet:text-[24px] text-c-section-heading">
           <p>
@@ -75,7 +82,10 @@ const ExploreSubsectionFour = () => {
         </h1>
       </div>
       <LargeImageCarousel>
+        <LargeImageCarousel.CarouselItemText items={images} />
         <LargeImageCarousel.CarouselItems images={images} />
+        <LargeImageCarousel.CarouselItemCaption items={images} />
+        <LargeImageCarousel.CarouselNav items={images} />
       </LargeImageCarousel>
     </SectionLayout>
   )
