e5ef7d8cb9ec5ef574b14e014535e0d09a07cbf5
Mason Hu <xm.hu.wk@gmail.com>
2023-11-21 19:39:29 +0200

    benefits section complete

--- components/carousel/small-image-carousel/small-image-carousel.tsx
+++ components/carousel/small-image-carousel/small-image-carousel.tsx
@@ -17,18 +17,18 @@ import ChevronArrow from '@/components/arrows/chevron-arrow';
 
 const pictureSize = (width: number) => {
   if (width < TABLET_SCREEN_SIZE) {
-    return 250;
+    return 200;
   }
 
   if (width < DESKTOP_SCREEN_SIZE) {
-    return 400;
+    return 250;
   }
 
-  return 600;
+  return 300;
 };
 
 const CarouselItem = (props: {
-  item: { url: string; text: string };
+  item: { url: string; text: string; title: string };
   itemIndex: number;
   getCarouselRef?: () => MutableRefObject<any>;
   setCurrentFocusedItemIndex: Function;
@@ -37,62 +37,67 @@ const CarouselItem = (props: {
 }) => {
   const { item, itemIndex, setCurrentFocusedItemIndex, currentFocusedItemIndex, carouselRef } =
     props;
-  //   const prevIndexRef = useRef(currentFocusedItemIndex);
+  const prevIndexRef = useRef(currentFocusedItemIndex);
   const itemRef = useRef<any>(null);
-  //   const { width } = useWindowDimensions();
-  //   const offset = width! * 0.5 + pictureSize(width!) / 2; // Halft of image width
-  //   const negOffset = width! * 0.5 - pictureSize(width!) / 2;
-  //   const { scrollXProgress } = useScroll({
-  //     container: carouselRef,
-  //     target: itemRef,
-  //     axis: 'x',
-  //     offset: [`start ${offset}px`, `end ${offset}px`, `start ${negOffset}px`, `end ${negOffset}px`],
-  //     layoutEffect: false,
-  //   });
-  //   const opacity = useTransform(scrollXProgress, [0, 0.35, 0.75, 1], [0.3, 1, 1, 0.3]);
-  //   const pictureWidth = pictureSize(width!);
-  //   const pictureHeight = pictureWidth / 1.3;
-  //   const height = useTransform(
-  //     scrollXProgress,
-  //     [0, 0.35, 0.75, 1],
-  //     [0.8 * pictureHeight, pictureHeight, pictureHeight, pictureHeight]
-  //   );
+  const { width } = useWindowDimensions();
+  const offset = width! * 0.5 + pictureSize(width!) / 2; // Halft of image width
+  const negOffset = width! * 0.5 - pictureSize(width!) / 2;
+  const { scrollXProgress } = useScroll({
+    container: carouselRef,
+    target: itemRef,
+    axis: 'x',
+    offset: [`start ${offset}px`, `end ${offset}px`, `start ${negOffset}px`, `end ${negOffset}px`],
+    layoutEffect: false,
+  });
+  const opacity = useTransform(scrollXProgress, [0, 0.35, 0.75, 1], [0.2, 1, 1, 0.2]);
+  const scale = useTransform(scrollXProgress, [0, 0.35, 0.75, 1], [0.85, 1, 1, 0.85]);
 
-  //   useEffect(() => {
-  //     if (currentFocusedItemIndex !== prevIndexRef.current && currentFocusedItemIndex === itemIndex) {
-  //       itemRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
-  //     }
-  //     prevIndexRef.current = currentFocusedItemIndex;
-  //   }, [currentFocusedItemIndex]);
+  useEffect(() => {
+    if (currentFocusedItemIndex !== prevIndexRef.current && currentFocusedItemIndex === itemIndex) {
+      itemRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+    }
+    prevIndexRef.current = currentFocusedItemIndex;
+  }, [currentFocusedItemIndex]);
 
-  //   useMotionValueEvent(opacity, 'change', (latestOpacity) => {
-  //     if (latestOpacity === 1) {
-  //       setCurrentFocusedItemIndex(itemIndex);
-  //       prevIndexRef.current = itemIndex;
-  //     }
-  //   });
+  useMotionValueEvent(opacity, 'change', (latestOpacity) => {
+    if (latestOpacity >= 0.9) {
+      setCurrentFocusedItemIndex(itemIndex);
+      prevIndexRef.current = itemIndex;
+    }
+  });
 
   return (
-    <li className="relative snap-center w-[250px] h-[250px]" ref={itemRef}>
-      <Image
-        src={item.url}
-        alt={`carousel image with caption: ${item.text}`}
-        fill
-        sizes="600px, 600px"
-        style={{
-          objectFit: 'cover',
-        }}
-      />
+    <li className="snap-center" ref={itemRef}>
+      <motion.div
+        style={{ opacity, scale }}
+        className="relative flex flex-col gap-3 max-w-[200px] tablet:max-w-[250px] desktop:max-w-[300px]"
+      >
+        <div className="relative w-[200px] h-[200px] tablet:w-[250px] tablet:h-[250px] desktop:w-[300px] desktop:h-[300px]">
+          <Image
+            src={item.url}
+            alt={`carousel image with caption: ${item.text}`}
+            fill
+            sizes="600px, 600px"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <h3 className="text-c-section-heading text-[18px] tablet:text-[20px]">{item.title}</h3>
+        <article className="text-c-heading text-[14px] tablet:text-[16px]">{item.text}</article>
+      </motion.div>
     </li>
   );
 };
 
 type SmallImageCarouselProps = PropsWithChildren & {
   images: { url: string; text: string; title: string }[];
+  setCurrentFocusedItemIndex: any;
+  currentFocusedItemIndex: number;
 };
 const SmallImageCarousel = (props: SmallImageCarouselProps) => {
-  const { images } = props;
-  const [currentFocusedItemIndex, setCurrentFocusedItemIndex] = useState(0);
+  const { images, currentFocusedItemIndex, setCurrentFocusedItemIndex } = props;
+  // const [currentFocusedItemIndex, setCurrentFocusedItemIndex] = useState(0);
   const carouselRef = useRef<any>(null);
   const { width } = useWindowDimensions();
 
@@ -111,13 +116,38 @@ const SmallImageCarousel = (props: SmallImageCarouselProps) => {
 
   // Pull scroll container to middle position
   useEffect(() => {
-    carouselRef.current.scrollLeft = width!;
+    const middleIndex = Math.floor(images.length / 2);
+    const containerWidth = width! < TABLET_SCREEN_SIZE ? 5 * width! : 2 * width!;
+    const pictureWidth = pictureSize(width!);
+    const edgeOffset = containerWidth - images.length * pictureWidth;
+    const scrollPix = edgeOffset / 2 + middleIndex * pictureWidth;
+    console.log(scrollPix);
+    carouselRef.current.scrollLeft = scrollPix;
   }, [carouselRef]);
 
   return (
-    <div className="relative w-[100vw] left-[calc(-50vw+50%)] overflow-x-scroll no-scrollbar snap-mandatory snap-x" ref={carouselRef}>
-      <ul className="flex justify-center w-[300vw] gap-[10px]">{itemList}</ul>
-    </div>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      viewport={{
+        once: true,
+        amount: 0.8,
+      }}
+      transition={{
+        delay: 0.5,
+        duration: 1,
+      }}
+      className="relative w-[100vw] left-[calc(-50vw+50%)] overflow-x-scroll no-scrollbar snap-mandatory snap-x"
+      ref={carouselRef}
+    >
+      <ul className="flex justify-center gap-[10px] w-[500vw] tablet:w-[300vw] tablet:gap-[10px]">
+        {itemList}
+      </ul>
+    </motion.div>
   );
 };
 
--- components/sections/benefits.tsx
+++ components/sections/benefits.tsx
@@ -1,82 +1,107 @@
 import SectionLayout from '../layout/section-layout';
 import SectionHeading from '../headings/section-heading';
 import InnerHeading from '../headings/inner-heading';
-import { motion } from 'framer-motion';
+import { motion, useAnimate } from 'framer-motion';
 import ChevronArrow from '../arrows/chevron-arrow';
 import SmallImageCarousel from '../carousel/small-image-carousel/small-image-carousel';
+import { useEffect, useRef, useState } from 'react';
+import ReadMoreButton from '../buttons/read-more-button';
 
 const images = [
   {
     // TODO: change to img1 after testing
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img7',
     text: 'Lorem ipsum, dolor sit amet consectetur desc1 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Lifestyle Estates'
+    title: 'Lifestyle Estates',
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img2',
     text: 'Lorem ipsum, dolor sit amet consectetur desc2 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Developers'
+    title: 'Developers',
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img3',
     text: 'Lorem ipsum, dolor sit amet consectetur desc3 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Real Estate Agents'
+    title: 'Real Estate Agents',
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img4',
     text: 'Lorem ipsum, dolor sit amet consectetur desc4 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Buyers'
+    title: 'Buyers',
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img5',
     text: 'Lorem ipsum, dolor sit amet consectetur desc5 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Commercial Property Rentals'
+    title: 'Commercial Property Rentals',
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img6',
     text: 'Lorem ipsum, dolor sit amet consectetur desc6 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Towers'
+    title: 'Towers',
   },
   {
     url: 'https://d1r0ovlr0podg3.cloudfront.net/imgs/benefits-img7',
     text: 'Lorem ipsum, dolor sit amet consectetur desc7 elit. Necessitatibus qui repudiandae consectetur beatae totam esse mollitia rem libero sit.',
-    title: 'Something else'
+    title: 'Something else',
   },
 ];
 
 const Benefits = () => {
+  const [currentFocusedItemIndex, setCurrentFocusedItemIndex] = useState(Math.floor(images.length / 2));
+  const [scope, animate] = useAnimate();
+
+  const handleChangeCarouselItem = (type: 'inc' | 'dec') => {
+    setCurrentFocusedItemIndex(prev => {
+      let currIndex = prev;
+      if(type === 'inc') {
+        currIndex = Math.min(images.length - 1, currIndex + 1);
+      } else {
+        currIndex = Math.max(0, currIndex - 1);
+      }
+      return currIndex;
+    })
+  };
+
+  const animateButtonForIndexChange = async () => {
+    await animate('svg', {scale: 1.1}, {duration: 0.2});
+    await animate('svg', {scale: 1}, {duration: 0.2});
+    await animate('svg', {scale: 1.1}, {duration: 0.2});
+    await animate('svg', {scale: 1.0}, {duration: 0.2});
+  }
+
+  useEffect(() => {
+    animateButtonForIndexChange();
+  }, [currentFocusedItemIndex])
+  
   return (
     <SectionLayout sectionName={'benefits'} bgColor="bg-c-primary" noPadding>
       <div className="relative pt-14 px-[5vw] tablet:px-[15vw] tablet:pt-[100px] tablet:pb-[50px]">
-        <div className='bg-c-secondary px-[5vw] tablet:px-[50px] max-w-[800px] mx-auto'>
+        <div className="flex flex-col gap-3 bg-c-secondary px-[5vw] py-[30px] laptop:py-[50px] tablet:px-[50px] max-w-[800px] mx-auto">
           {/* Heading portion */}
           <SectionHeading text="Benefits" />
-          <div className='flex flex-col justify-center mt-[10px] gap-[10px] tablet:flex-row tablet:justify-between items-center'>
+          <div className="flex flex-col justify-center gap-[10px] mb-[10px] tablet:mb-[20px] tablet:flex-row tablet:justify-between items-center">
             <InnerHeading text="How can we help you" />
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{
-                opacity: 1
+                opacity: 1,
               }}
               transition={{ duration: 1 }}
               className="relative flex justify-center gap-10 mt-[10px] tablet:gap-3 tablet:mt-0"
             >
-              <ChevronArrow
-                direction="left"
-                size="30px"
-                inactive={false}
-                onClick={() => {}}
-              />
-              <ChevronArrow
-                direction="right"
-                size="30px"
-                inactive={false}
-                onClick={() => {}}
-              />
+              <ChevronArrow direction="left" size="30px" inactive={currentFocusedItemIndex === 0} onClick={handleChangeCarouselItem.bind(null, 'dec')} />
+              <ChevronArrow direction="right" size="30px" inactive={currentFocusedItemIndex === images.length - 1} onClick={handleChangeCarouselItem.bind(null, 'inc')} />
             </motion.div>
           </div>
-        {/* Carousel portion */}
-        <SmallImageCarousel images={images} />
+          {/* Carousel portion */}
+          <SmallImageCarousel
+            images={images}
+            currentFocusedItemIndex={currentFocusedItemIndex}
+            setCurrentFocusedItemIndex={setCurrentFocusedItemIndex}
+          />
+          <div className='w-[100%] flex justify-center laptop:mt-5' ref={scope}>
+            <ReadMoreButton />
+          </div>
         </div>
       </div>
     </SectionLayout>
