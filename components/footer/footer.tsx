9944f2519fdeef15c4602fceffd0b711c67a10f8
Mason <xm.hu.wk@gmail.com>
2024-09-18 21:46:28 +0200

    add privacy policy in footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import instagram from '../../public/imgs/instagram_logo.png';
 import linkedin from '../../public/imgs/linkedin_logo.png';
 import facebook from '../../public/imgs/facebook_logo.png';
 import Link from 'next/link';
 
+const privacyPolicyUrl = 'https://d1r0ovlr0podg3.cloudfront.net/doc/privacy-policy';
+
 const Footer = () => {
   return (
-    <footer className="h-[90px] bg-[#101010] flex flex-col justify-center items-center gap-2 tablet:flex-row tablet:gap-8">
+    <footer className="py-5 bg-[#101010] flex flex-col justify-center items-center gap-2 tablet:flex-row tablet:gap-8">
       <p className="text-c-heading text-[20px]">
         <span>Follow us </span>
-        <span className="text-c-section-heading">on our social media channels:</span>
+        <span className="text-c-section-heading">on our social media channels</span>
       </p>
       <div className="flex gap-8">
         <Link
@@ -28,6 +30,17 @@ const Footer = () => {
           <Image src={instagram} alt="Instagram social" width={30} />
         </Link>
       </div>
+      <span className="hidden text-c-section-heading text-[25px] tablet:inline">|</span>
+      <hr className="w-[70%] text-c-section-heading my-3 tablet:hidden"/>
+      <Link
+        href={privacyPolicyUrl}
+        target='_blank'
+        rel="noopener noreferrer"
+        download="privacy-policy.pdf"
+        className="text-c-section-heading text-[20px]"
+      >
+        privacy policy
+      </Link>
     </footer>
   );
 };
